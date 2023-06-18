becc624f0ca1372c455f736542d9e807f4587bd6
Mayuresh Bhardwaj <mbhardwaj1@hotmail.com>
2023-06-17 21:30:10 -0400

    updates

--- generate_release_notes.py
+++ generate_release_notes.py
@@ -15,16 +15,39 @@ def generate_release_notes(commit_id_1, commit_id_2):
     
     openai.api_key = os.environ["OPENAI_API_KEY"]  # Retrieve API key from environment variable
 
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {"role": "system", "content": "/start"},
-            {"role": "user", "content": prompt}
-        ]
-    )
+    # Split the prompt into chunks to fit within the model's maximum context length
+    prompt_chunks = split_prompt(prompt, max_tokens=4096)
+    response_chunks = []
 
-    release_notes = response.choices[0].message.content.strip()
+    for chunk in prompt_chunks:
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": "/start"},
+                {"role": "user", "content": chunk}
+            ]
+        )
+        response_chunks.append(response.choices[0].message.content)
+
+    release_notes = " ".join(response_chunks).strip()
     return release_notes
+
+def split_prompt(prompt, max_tokens):
+    prompt_tokens = prompt.split()
+    chunks = []
+    current_chunk = ""
+
+    for token in prompt_tokens:
+        if len(current_chunk) + len(token) < max_tokens:
+            current_chunk += token + " "
+        else:
+            chunks.append(current_chunk.strip())
+            current_chunk = token + " "
+    
+    if current_chunk:
+        chunks.append(current_chunk.strip())
+    
+    return chunks
 
 if __name__ == "__main__":
     commit_id_1 = os.environ.get("COMMIT_ID_1")
